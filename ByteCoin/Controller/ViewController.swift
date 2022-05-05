560f93972011ebdaea266db29d49756dd2ea113c
julienshim <25723428+julienshim@users.noreply.github.com>
2022-05-04 21:29:10 -0700

    first

---
+++ ByteCoin/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Julien Shim on 5/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,18 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by Julien Shim on 5/4/22.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+    
+}
